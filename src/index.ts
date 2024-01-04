a032c8f7f459c5d9f80483014f9c401b11b134df
mirza <mirza.akhena@gmail.com>
2024-01-04 15:19:28 +0700

    simplify application

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
+// import { helloRouter } from "./app/controller";
+// app.use(helloRouter);
+
 import express from "express";
-import { helloRouter } from "./app/controller";
 
 const app = express();
 
-app.use(helloRouter);
+app.get("/", (req, res) => {
+  // res.send(process.env.MESSAGE || "alhamdulillah");
+
+  console.log("API called");
+  res.json({ message: "Alhamdulillah" });
+});
 
 app.listen(3000, () => {
   console.log("App listening on port 3000!");
